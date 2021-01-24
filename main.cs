6f342aa1f73761479f553d93be3fc19a81ad112f
Damngineer <77919779+Damngineer@users.noreply.github.com>
2021-01-24 15:14:11 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,2 +1,29 @@
-Console.WriteLine("Hello World!");
-sa
+  
+using System;
+
+namespace ödev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            musteri musteri1 = new musteri();
+            musteri1.id = 21895855;
+            musteri1.name = "berk";
+            musteri1.sName = "saglicak";
+
+            musteri musteri2 = new musteri();
+            musteri1.id = 21895558;
+            musteri1.name = "kubilay";
+            musteri1.sName = "arslan";
+
+
+            musteriManager manager = new musteriManager();
+
+            manager.ekle(musteri1);
+            manager.ekle(musteri2);
+            manager.sil(musteri2);
+        }
+    }
+}
